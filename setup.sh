b4576997baf59a76a00731017cea930af8d7fc87
xcs491 <keith.klassen@gmail.com>
2019-06-30 13:12:53 -0600

    Modified setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get install git neovim zsh tmux
+sudo apt-get update; sudo apt-get upgrade -y; sudo apt-get install git neovim zsh tmux -y
 mkdir ~/.config/nvim; mkdir ~/gits; mkdir ~/.config/nvim/autoload
 cd ~/gits; git clone https://github.com/xcs491/VimEnvironment.git
 cd ~/.config/nvim/autoload; wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp init.vim ~/.config/nvim; cp tconfig.sh ~/
-cd ~/; sudo sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cd ~/; sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ~/gits/VimEnvironment/aaron.zsh-theme ~/.oh-my-zsh/themes/
 cp ~/gits/VimEnvironment/.zshrc ~/
+chsh -s /usr/bin/zsh
